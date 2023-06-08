e36602b6fca940bb4127d2cfccdec5130804935d
bhuwan <bkspoudel@gmail.com>
2023-06-08 12:28:26 +0200

    added a header section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { HeaderSectionComponent } from './components/header-section/header-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    TasksComponent,
+    HeaderSectionComponent
   ],
   imports: [
     BrowserModule,
